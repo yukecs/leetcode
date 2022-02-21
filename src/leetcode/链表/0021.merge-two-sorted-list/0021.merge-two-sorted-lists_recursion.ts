9448c817231bf7929da50a5ee2f83f6bbc3c3064
musksy <3571585812@qq.com>
2022-02-21 18:28:30 +0800

    Start doing question 82

--- src/leetcode/链表/0021.merge-two-sorted-list/0021.merge-two-sorted-lists_recursion.ts
+++ src/leetcode/链表/0021.merge-two-sorted-list/0021.merge-two-sorted-lists_recursion.ts
@@ -1,11 +1,14 @@
-// class ListNode {
-//     val: number
-//     next: ListNode | null
-//     constructor(val?: number, next?: ListNode | null) {
-//         this.val = (val === undefined ? 0 : val)
-//         this.next = (next === undefined ? null : next)
-//     }
-// }
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 import {ListNode} from '../../type'
 
 /**递归 参考答案 */
