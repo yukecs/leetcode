041b10e62755378275ceadc3344908cb6d3a5074
musksy <3571585812@qq.com>
2022-02-26 18:44:12 +0800

    0082-done

--- src/leetcode/链表/0082.remove-duplicates-from-sorted-list-ii/0082.remove-duplicates-from-sorted-list-ii_iterate.ts
+++ src/leetcode/链表/0082.remove-duplicates-from-sorted-list-ii/0082.remove-duplicates-from-sorted-list-ii_iterate.ts
@@ -12,7 +12,7 @@
 
 import {ListNode} from '../../type'
 
-/**迭代 first success*/
+/**迭代 first success 时间复杂度(2n)*/
 export function deleteDuplicates(head: ListNode | null): ListNode | null {
     var prehead = head
     var set = new Set<number>()
@@ -38,4 +38,25 @@ export function deleteDuplicates(head: ListNode | null): ListNode | null {
     return l1.next
 };
 
+/**迭代 参考答案 时间复杂度(n)*/
+var deleteDuplicates_2 = function(head: ListNode | null): ListNode | null {
+    if (!head) {
+        return head;
+    }
+
+    const dummy = new ListNode(0, head);
+
+    let cur = dummy;
+    while (cur.next && cur.next.next) {
+        if (cur.next.val === cur.next.next.val) {
+            const x = cur.next.val;
+            while (cur.next && cur.next.val === x) {
+                cur.next = cur.next.next;
+            } 
+        } else {
+            cur = cur.next;
+        }
+    }
+    return dummy.next;
+};
 
