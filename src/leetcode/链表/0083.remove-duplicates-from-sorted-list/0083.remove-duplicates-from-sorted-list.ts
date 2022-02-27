d33ddd52310078a74c1d5f25d1b2493bfeb11e0b
tingyuzhucha <yukecs@github.com>
2022-02-27 22:12:44 +0800

    0083-first-success

--- src/leetcode/链表/0083.remove-duplicates-from-sorted-list/0083.remove-duplicates-from-sorted-list.ts
+++ src/leetcode/链表/0083.remove-duplicates-from-sorted-list/0083.remove-duplicates-from-sorted-list.ts
@@ -10,8 +10,24 @@
  * }
  */
 
- import {ListNode} from '../../type'
+import { ListNode } from '../../type'
 
- export function deleteDuplicates(head: ListNode | null): ListNode | null {
-    return new ListNode(-1)
- };
+/** 迭代 fisrt success */
+export function deleteDuplicates(head: ListNode | null): ListNode | null {
+   if(!head){
+      return null
+   }
+   let prehead = new ListNode(-1)
+   let prev = head
+   while (prev.next !== null) {
+      prehead = prev.next
+      while (prehead && prehead.val === prev.val) {
+         prehead = prehead.next
+      }
+      prev.next = prehead
+      if(prev.next){
+         prev = prev.next
+      }
+   }
+   return head
+};
