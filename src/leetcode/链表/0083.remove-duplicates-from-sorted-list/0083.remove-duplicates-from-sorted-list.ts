5f44e7842aca36d30fed6df3eb156234396b7543
tingyuzhucha <yukecs@github.com>
2022-02-27 22:17:31 +0800

    0083-done

--- src/leetcode/链表/0083.remove-duplicates-from-sorted-list/0083.remove-duplicates-from-sorted-list.ts
+++ src/leetcode/链表/0083.remove-duplicates-from-sorted-list/0083.remove-duplicates-from-sorted-list.ts
@@ -31,3 +31,16 @@ export function deleteDuplicates(head: ListNode | null): ListNode | null {
    }
    return head
 };
+
+/** 迭代 参考答案 */
+export const deleteDuplicates_2 = function(head: ListNode | null): ListNode | null {
+   var cur = head;
+   while(cur && cur.next) {
+       if(cur.val == cur.next.val) {
+           cur.next = cur.next.next;
+       } else {
+           cur = cur.next;
+       }
+   }
+   return head;
+};
