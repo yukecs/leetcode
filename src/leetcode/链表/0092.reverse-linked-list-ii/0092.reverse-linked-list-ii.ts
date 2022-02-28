0be991710c59e98cb60b5470f8e8e7873187fa1f
musksy <3571585812@qq.com>
2022-02-28 17:44:02 +0800

    checkpoint-20220228

--- src/leetcode/链表/0092.reverse-linked-list-ii/0092.reverse-linked-list-ii.ts
+++ src/leetcode/链表/0092.reverse-linked-list-ii/0092.reverse-linked-list-ii.ts
@@ -10,8 +10,28 @@
  * }
  */
 
- import { ListNode } from '../../type'
+import { ListNode } from '../../type'
 
- function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
+export function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
+    if(!head||left === right){
+        return head
+    }
+    let l = new ListNode(-1,head)
+    let prehead = l;
+    for(let i = 1 ; i<left; i++){
+        prehead = prehead.next
+    }
 
+    /**左边 */
+    let l1 = prehead
+
+    let cur =l1.next
+    let rL:ListNode | null = null
+    for(let i = 0;i<right-left;i++){
+        rL = new ListNode(cur.val,rL)
+        cur = cur.next
+    }
+    l1.next = rL
+    
+    return l.next
 };
