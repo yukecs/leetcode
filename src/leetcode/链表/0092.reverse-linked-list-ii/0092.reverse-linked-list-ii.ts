1ebda9a9aeb3d52d35ad6ef7843be7d2cd7bf4aa
musksy <3571585812@qq.com>
2022-03-01 10:30:48 +0800

    0092-done

--- src/leetcode/链表/0092.reverse-linked-list-ii/0092.reverse-linked-list-ii.ts
+++ src/leetcode/链表/0092.reverse-linked-list-ii/0092.reverse-linked-list-ii.ts
@@ -12,26 +12,50 @@
 
 import { ListNode } from '../../type'
 
-export function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
-    if(!head||left === right){
-        return head
-    }
-    let l = new ListNode(-1,head)
-    let prehead = l;
-    for(let i = 1 ; i<left; i++){
-        prehead = prehead.next
+/**迭代 参考答案 */
+export var reverseBetween = function(head:ListNode | null, left:number, right:number) {
+    // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+    const dummyNode = new ListNode(-1);
+    dummyNode.next = head;
+
+    let pre = dummyNode;
+    // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+    // 建议写在 for 循环里，语义清晰
+    for (let i = 0; i < left - 1; i++) {
+        pre = pre.next;
     }
 
-    /**左边 */
-    let l1 = prehead
-
-    let cur =l1.next
-    let rL:ListNode | null = null
-    for(let i = 0;i<right-left;i++){
-        rL = new ListNode(cur.val,rL)
-        cur = cur.next
+    // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+    let rightNode = pre;
+    for (let i = 0; i < right - left + 1; i++) {
+        rightNode = rightNode.next;
     }
-    l1.next = rL
-    
-    return l.next
+
+    // 第 3 步：切断出一个子链表（截取链表）
+    let leftNode = pre.next;
+    let curr = rightNode.next;
+
+    // 注意：切断链接
+    pre.next = null;
+    rightNode.next = null;
+
+    // 第 4 步：同第 206 题，反转链表的子区间
+    reverseLinkedList(leftNode);
+
+    // 第 5 步：接回到原来的链表中
+    pre.next = rightNode;
+    leftNode.next = curr;
+    return dummyNode.next;
 };
+
+const reverseLinkedList = (head) => {
+    let pre = null;
+    let cur = head;
+
+    while (cur) {
+        const next = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = next;
+    }
+}
